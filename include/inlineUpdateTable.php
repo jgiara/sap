9d6d48001ba232054d133d18f20c5d2160c419a3
Jon Giara <giara@bc.edu>
2017-01-08 17:39:44 -0500

    inline updates done and working

--- include/inlineUpdateTable.php
+++ include/inlineUpdateTable.php
@@ -1,27 +1,16 @@
 <?php
+	require_once '../../resources/initTableFunctions.php';
 
-		$id = $_POST['id'];
-		$field = $_POST['field'];
-		$table = $_POST['table'];
-		$newValue = $_POST['newValue'];
-		$whereField = $_POST['whereField'];
+	$id = $_POST['id'];
+	$field = $_POST['field'];
+	$table = $_POST['table'];
+	$newValue = $_POST['newValue'];
+	$whereField = $_POST['whereField'];
 
 
-		$dbc = @mysqli_connect("localhost", "root", "root", "SAP")
-	       or die("Could not open db SAP, " . mysqli_connect_error());
-		$query = "Update $table set $field = '$newValue' where $whereField = '$id'";				
-		
-		
-		if(mysqli_query($dbc, $query)) {
-			$result = true;
-		}
-		else {
-			$result = false;
-		}
-		//Add row to audit table - or add a stored procedure?
-	
-		$data_from_post = array('update' => $field);
-		echo json_encode($result);
-		mysqli_close($dbc);
+	$items = $fns->inlineUpdate($id, $field, $table, $newValue, $whereField);
+
+	echo json_encode($items);
+	$db = null;
 ?>
 ?>
